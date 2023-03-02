a05be6e9461ef012f5ba58549555f4e5371fc196
ijlik <ijlik.k5s@gmail.com>
2023-03-02 11:00:40 +0700

    Update bot

--- app/Traits/OpenAi.php
+++ app/Traits/OpenAi.php
@@ -2,41 +2,28 @@
 
 namespace App\Traits;
 
+use Illuminate\Support\Facades\Http;
+
 trait OpenAi
 {
     public function generateText($field, $title)
     {
-        $json_request = '{
-              "model": "text-davinci-003",
-              "prompt": ' . $this->makePrompt($title, $field) . ',
-              "temperature": 0.7,
-              "max_tokens": ' . $this->getMaxToken($field) . ',
-              "top_p": 1,
-              "frequency_penalty": 0,
-              "presence_penalty": 0
-            }';
+        $postData = [
+            "model" => "text-davinci-003",
+            "prompt" => $this->makePrompt($title, $field),
+            "temperature" => 0.7,
+            "max_tokens" => $this->getMaxToken($field),
+            "top_p" => 1,
+            "frequency_penalty" => 0,
+            "presence_penalty" => 0
+        ];
 
-        $curl = curl_init();
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => 'https://api.openai.com/v1/completions',
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => '',
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => 'POST',
-            CURLOPT_POSTFIELDS => $json_request,
-            CURLOPT_HTTPHEADER => array(
-                'Content-Type: application/json',
-                'Authorization: Bearer ' . env('OPEN_AI_API_KEY')
-            ),
-        ));
+        $response = Http::withHeaders([
+            'Content-Type' => 'application/json',
+            'Authorization' => 'Bearer ' . env('OPEN_AI_API_KEY')
+        ])->post('https://api.openai.com/v1/completions', $postData);
 
-        $response = curl_exec($curl);
-
-        curl_close($curl);
-        return json_decode($response, true);
+        return $response->json();
     }
 
     private function makePrompt($title, $field): string
